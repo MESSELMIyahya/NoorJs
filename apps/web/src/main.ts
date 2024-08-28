8e99645bdadf552e1b645dd842bdc7acea7654be
MESSELMII_YAHYA <107892703+MESSELMIyahya@users.noreply.github.com>
2024-08-28 12:03:39 +0100

    Add the web app for testing the core package with simple component tests

--- apps/web/src/main.ts
+++ apps/web/src/main.ts
@@ -1,22 +1,89 @@
-import { Header } from "@repo/ui/header";
-import "./style.css";
-import typescriptLogo from "/typescript.svg";
-import { Counter } from "@repo/ui/counter";
-import { setupCounter } from "@repo/ui/setup-counter";
+import {
+  createRoot,
+  Component,
+  renderRoot,
+  element,
+  state,
+  FCRendered,
+} from "@repo/core";
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    ${Header({ title: "Web" })}
-    <div class="card">
-      ${Counter()}
-    </div>
-  </div>
-`;
+// creating the root
+const root = createRoot();
 
-setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
+// button component
+
+function ButtonComponent(this: Component): FCRendered {
+  element("button", this);
+
+  let times = state(0, this);
+
+  this.setEvent("onClick", (e) => {
+    console.log("clicked :", times);
+    times++;
+    if (times == 6) {
+      this.removeEvent("onClick");
+    } else {
+      this.render({ times },`increased {{times}}`);
+    }
+  });
+
+  console.log(this.get("id"));
+
+  return [{ times }, `count {{times}}`];
+}
+
+// render the component
+
+renderRoot(root, [ButtonComponent]);
+
+// const element = () => ({});
+
+// function Componenft() {
+//   element("div", this);
+//   this.inject(["Navbar"], this);
+//   const name = state("name", this);
+
+//   //   const [ref, element] = accessElement(this);
+
+//   function fetch() {
+//     return [1, 2, 3, 4];
+//   }
+
+//   this.addMethod("onclick", (ele) => {
+//     ele.innerText = name;
+//   });
+
+//   this.click();
+
+//   return /*html*/ `
+//     <p><Navbar /></p>
+//     <p id="${""}">{name}</p>
+//     { age }`;
+// }
+
+// console.log(Component.apply({}));
+
+// function Component(this:{name:string},n) {
+//     element("p", this);
+
+//     console.log(this,n);
+
+//     this.style({
+//         name:''
+//     })
+
+//     this.classNames('')
+
+//     const count = state("count", this);
+
+//     const increment = ()=>{
+//         count++
+//     }
+
+//     this.addMethod("onclick", (ele) => {
+//       count++;
+//     //   this.setInnerText(count);
+//     },true);
+
+//     return /*html*/ `{count}`;
+// }
