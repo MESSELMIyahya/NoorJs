37b7b9445cf1f247aa8fd1340e4634aa3dd523eb
Yahya Messelmi <yahyaaoo200@gmail.com>
2024-08-31 18:15:36 +0100

    Add simple counting  with the setView method

--- apps/web/src/app.ts
+++ apps/web/src/app.ts
@@ -1,63 +1,23 @@
 import { Component, element, FCRendered } from "@repo/core";
 
 function App(this: Component): FCRendered {
-  element("p", this);
+  element("button", this);
 
-  let [getCount, setCount, ref] = this.state(0);
-  let [getAge, setAge, ageRef] = this.state(20);
+  let [getCount, setCount, countRef] = this.state(0);
 
   this.onChange(() => {
-    console.log("count changed", getCount());
-    if (getCount() / 5 == 1) {
-      setAge(getCount() + 3);
+    // render when the count state gets changed
+    this.render({ count: getCount() });
+    if (getCount() == 10) {
+      this.setView(`You won ${getCount()}`);
     }
-  }, [ref]);
+  }, [countRef]);
 
-  this.onChange(() => {
-    console.log("age changed", getAge());
-  }, [ageRef]);
-
-  const buttonsIds = {
-    b1: this.get("id") + "b1",
-    b2: this.get("id") + "b2",
-  };
-
-  this.onMount(() => {
-    const button1 = document.getElementById(buttonsIds.b1)!;
-    const button2 = document.getElementById(buttonsIds.b2)!;
-    // set the methods
-    button1.onclick = increment;
-    button2.onclick = decrement;
-  });
-
-  this.onRender(() => {
-    const button1 = document.getElementById(buttonsIds.b1)!;
-    const button2 = document.getElementById(buttonsIds.b2)!;
-    // set the methods
-    button1.onclick = increment;
-    button2.onclick = decrement;
-  });
-
-  const increment = () => {
+  this.setEvent("onClick", () => {
     setCount((c) => c + 1);
-    this.render({ count: getCount() });
-  };
+  });
 
-  const decrement = () => {
-    setCount(getCount() - 1);
-    this.render({ count: getCount() });
-  };
-
-  setAge(1);
-
-  return [
-    { count: getCount() },
-    /*html*/ `
-    <p>{{count}}</p>
-    <button id="${buttonsIds.b1}">increase</button>
-    <button id="${buttonsIds.b2}">decrease</button>
-    `,
-  ];
+  return [{ count: getCount() }, /*html*/ `{{count}}`];
 }
 
 export default App;
