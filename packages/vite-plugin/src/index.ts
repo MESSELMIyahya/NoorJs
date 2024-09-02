5fd7516e67b3e8e534e17bdcddc34b3325bcca80
Yahya Messelmi <yahyaaoo200@gmail.com>
2024-09-02 14:29:47 +0100

    Add the NoorJS vite plugin that modifys  the vite's esbuild config to compali the JSX

--- packages/vite-plugin/src/index.ts
+++ packages/vite-plugin/src/index.ts
@@ -1,3 +1,22 @@
-const NoorPlugin = "plugin";
+import { PluginOption, UserConfig } from "vite";
 
-export default NoorPlugin;
+type NoorJsType = () => PluginOption;
+
+const NoorJs: NoorJsType = () => {
+  return {
+    name: "NoorJs",
+    config: (c) => {
+      //  custom esBuild options
+      const esbuildOptions: UserConfig["esbuild"] = {
+        jsxInject: "import {ComponentCreator} from '@noorJs/core'",
+        jsxFactory: "ComponentCreator",
+        // jsx: "preserve",
+      };
+      //   setting the JSX esBuild options to the custom options
+      c.esbuild = { ...c.esbuild, ...esbuildOptions };
+    },
+  };
+};
+
+export default NoorJs;
+// npm i @vitejs/plugin-react-swc
