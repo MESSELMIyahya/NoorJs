115b6e8a08697ff7c14b49eba23584139e00e49a
Yahya Messelmi <yahyaaoo200@gmail.com>
2024-09-07 23:06:08 +0100

    fix(web): Fix the 'web' build error with the path package

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,6 +1,17 @@
 import { defineConfig } from "vite";
 import NoorJs from "@noorjs/vite";
+import path from "path";
 
 export default defineConfig({
   plugins: [NoorJs()],
+  resolve: {
+    alias: {
+      "@noorJs/core": path.resolve(__dirname, "../../packages/core"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: ["@noorJs/core"],
+    },
+  },
 });
