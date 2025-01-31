699d9b4b334f66d3e802904e65353be6ea05dfa0
Yahya Messelmi <yahyaaoo200@gmail.com>
2025-01-31 21:28:46 +0100

    feat(Enums): :sparkles: Add the errors enum list

---
+++ packages/core/src/enums/errors.list.ts
@@ -0,0 +1,6 @@
+// __The NoorJs Errors Messages List__
+
+export enum NoorErrorsMessagesList {
+  // class component errors
+  PAIN_METHOD_NOT_FOUND = "The 'Paint' method is required is the component class",
+}
