ba7ec0a59fe2086dbc45fc8bbf226b24ec225636
Yahya Messelmi <yahyaaoo200@gmail.com>
2025-01-31 21:56:32 +0100

    feat(web): :sparkles: Add a simple component

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,3 +1,44 @@
-import { AddTwo } from '@noorjs/core'
+import { Component } from "@noorjs/core";
 
-console.log('add :',AddTwo(1,34))
+@Component()
+class AppComponent {
+  paint() {
+    return "Hello World";
+  }
+}
+
+// @Component({})
+// class MyComponent {
+//   isFetching = withState(false);
+//   data = withState([]);
+
+//   @OnInit() // will be ran called after the initial render
+//   fetching_data() {
+//     // set the isFetching state to 'true'
+//     this.isFetching.set(true);
+//     // ...fetch data logic
+//     // save the data in the state
+//     this.data.set([
+//       /* ...fetched data */
+//     ]);
+//     // set the isFetching state to 'false'
+//     this.isFetching.set(false);
+//   }
+
+//   // onChange
+//   @OnChange([data])
+//   fetch_data() {
+//     console.log("DATA IS FETCHED :", this.data.get());
+//   }
+
+//   return() {
+//     return (
+//       <div>
+//         {/* ..wrapping the fetched data */}
+//         {this.data.get().map((item) => (
+//           <Component /*...Props */ />
+//         ))}
+//       </div>
+//     );
+//   }
+// }
