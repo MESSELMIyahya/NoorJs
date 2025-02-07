adf5c4fac5d4c6a9bccb4be0f718108c7d18fda3
Yahya Messelmi <yahyaaoo200@gmail.com>
2025-02-07 10:52:04 +0100

    feat(Enums): :sparkles: Add new error message

--- packages/core/src/enums/errors.list.ts
+++ packages/core/src/enums/errors.list.ts
@@ -3,4 +3,7 @@
 export enum NoorErrorsMessagesList {
   // class component errors
   PAIN_METHOD_NOT_FOUND = "The 'Paint' method is required is the component class",
+  
+  // component & elements errors
+  COMPONENT_NOT_VALID = "The Pass Component Is Not Valid Or Hasn't Initialed By The '@Component()' Decorator"
 }
