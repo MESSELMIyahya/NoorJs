f80c4a4b8ecfcd60ccc4a878fe37dcbad26ab364
Yahya Messelmi <yahyaaoo200@gmail.com>
2025-02-07 11:01:46 +0100

    feat(vite): :sparkles: Edit the plugin to compile the 'createElement' and the old 'componentCreator'

--- packages/vite-plugin/src/index.ts
+++ packages/vite-plugin/src/index.ts
@@ -2,17 +2,17 @@ import { PluginOption, UserConfig } from "vite";
 
 type NoorJsType = () => PluginOption;
 
-const NoorJs: NoorJsType = () => {
+const NoorJs: NoorJsType = (old: boolean = false) => {
   return {
     name: "NoorJs",
     config: (c) => {
       //  custom esBuild options
       const esbuildOptions: UserConfig["esbuild"] = {
-        jsxInject: "import { ComponentCreator as _h } from '@noorjs/core'",
+        jsxInject: `import { ${old ? "componentCreator" : "createElement"} as _h } from '@noorjs/core'`,
         jsxFactory: "_h",
         // jsx: "preserve",
       };
-      //   setting the JSX esBuild options to the custom options
+      //  setting the JSX esBuild options to the custom options
       c.esbuild = { ...c.esbuild, ...esbuildOptions };
     },
   };
