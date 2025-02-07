1ea21822eb5ee874eba91917407cefc84a57bccd
Yahya Messelmi <yahyaaoo200@gmail.com>
2025-02-07 10:52:57 +0100

    feat(Symbols): :sparkles: Add the some noor symbols

---
+++ packages/core/src/global/symbols.list.ts
@@ -0,0 +1,8 @@
+// The Noor Symbols List
+
+export const NoorSymbolsList = {
+  // class component element Symbols
+  CLASS_COMPONENT_ELEMENT_SYMBOL: Symbol.for("noor-class-component"),
+  // element symbols
+  ELEMENT_SYMBOL: Symbol.for("noor-element"),
+};
