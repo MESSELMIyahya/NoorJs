9a48775744191768f7526acac9bb140c389fe29c
Yahya Messelmi <yahyaaoo200@gmail.com>
2025-02-21 22:29:37 +0100

    feat(Shared): :sparkles: Add the Noor Error class to the shared utilities

---
+++ packages/core/src/utilities/error/index.ts
@@ -0,0 +1,29 @@
+// __The NoorJs Error Class__
+
+// Error types
+type ErrorTypes = "NotCalled" | "NotFound" | "NotInitialized" | "NotValid";
+
+// ErrorNames
+const ErrorNames: Record<ErrorTypes, string> = {
+  NotCalled: "NoorJs:FunctionIsn'tCalled",
+  NotFound: "NoorJs:NotFound",
+  NotValid: "NoorJs:NotValid",
+  NotInitialized: "NoorJs:NotInitialized",
+};
+
+class NoorError extends Error {
+  constructor(message: string, ErrorType: ErrorTypes = "NotFound") {
+    super(message);
+    this.message = `${this.get_error_name(ErrorType)} (${message})`;
+    this.name = "NoorJs";
+  }
+
+  // _method to get the error name using the error type
+  get_error_name(type: ErrorTypes) {
+    // names based on the types
+    return ErrorNames[type];
+  }
+}
+
+// Export he Error
+export default NoorError;
