3ca3caef00ea77cf092af88c9ef88b07d257b75d
Yahya Messelmi <yahyaaoo200@gmail.com>
2025-02-21 22:33:04 +0100

    feat(DOM): :pencil2: Edit the render root function to check the rootElement if it's not valid and renders the root using the render function

--- packages/dom/src/factories/render-root/logic.ts
+++ packages/dom/src/factories/render-root/logic.ts
@@ -1,9 +1,22 @@
 // __ The NoorJs render root function logic __
+import { NoorError } from "@noorjs/shared/global";
 import { _NoorElementType } from "@noorjs/types";
+import { render } from "../render";
 
 // the render root function logic
 
 export function renderRoot(root: _NoorElementType, rootElement: HTMLElement) {
-  //
-  console.log("ROOT : ", root);
+  // check if the room element is valid
+  if (!rootElement) {
+    throw new NoorError(
+      "The Root Element Should Be Valid HTML Element",
+      "NotValid"
+    );
+  }
+
+  // clear the element inner html
+  rootElement.innerHTML = "";
+
+  // render the root
+  render(root.type, root.props, rootElement);
 }
