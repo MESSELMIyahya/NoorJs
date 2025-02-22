dc8e9ca6c4979505b1d9674f068db4255baf4960
Yahya Messelmi <yahyaaoo200@gmail.com>
2025-02-22 08:21:46 +0100

    fix(Types): :pencil2: Edit the _NoorComponentType and fix the types

--- packages/types/types/interfaces/component.ts
+++ packages/types/types/interfaces/component.ts
@@ -1,9 +1,11 @@
+import _NoorClassComponentInterface from "./class-component";
 import _NoorClassComponentContextType from "./component-context";
 
 // _The Noor Js Component Function Type
 
 // type
-export type _NoorComponentType = Function & {
+export type _NoorComponentType = {
+  new (...args: any[]): _NoorClassComponentInterface;
   property: {
     _$ctx: _NoorClassComponentContextType;
     [key: string]: any;
